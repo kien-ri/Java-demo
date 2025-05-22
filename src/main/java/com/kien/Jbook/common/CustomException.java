a30ca2044c220d9a4463d1f3b6198c766ecaf449
kien.ri <kien.ri@rightcode.co.jp>
2025-05-22 16:41:19 +0900

    カスタムエラークラスリファクタ

---
+++ src/main/java/com/kien/Jbook/common/CustomException.java
@@ -0,0 +1,18 @@
+package com.kien.Jbook.common;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private HttpStatus httpStatus;
+    private String field;
+    private Object value;
+
+    public CustomException(String message, HttpStatus httpStatus, String field, Object value) {
+        super(message);
+        this.httpStatus = httpStatus;
+        this.field = field;
+        this.value = value;
+    }
+}
--- src/main/java/com/kien/Jbook/common/exception/CustomException.java
+++
@@ -1,7 +0,0 @@
-package com.kien.Jbook.common.exception;
-
-public class CustomException extends RuntimeException {
-    public CustomException(String message) {
-        super(message);
-    }
-}
